8b221c61ffc039d7ab67cf9217f56d0c9be6e394
Unice17 <adenaikeeunice@gmail.com>
2022-12-09 14:29:10 -0800

    all files for the task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Lazador
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char a = 'a';
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar (a);
+	}
+	putchar ('\n');
+	return (0);
+}
