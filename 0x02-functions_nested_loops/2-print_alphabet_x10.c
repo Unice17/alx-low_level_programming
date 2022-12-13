06b1a9fdf993502ef781bb4ae8ca8bd8abb5a661
Unice17 <adenaikeeunice@gmail.com>
2022-12-13 14:04:59 -0800

    c nested project

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * main - check the code.
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+int main(void)
+
+{
+
+	    print_alphabet_x10();
+
+	        return (0);
+
+}
+
+{
+
+	char i, l;
+
+
+
+	for (l = 0; l <= 9; l++)
+
+	{
+
+		for (i = 'a'; i <= 'z'; i++)
+
+			{
+
+				_putchar(i);
+			}													}
+
+
+	_putchar('\n');
+
+										}
+
+}
