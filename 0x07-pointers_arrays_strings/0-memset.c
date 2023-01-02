fc818871e398cae9d9b24efac636b21fd868b636
Unice17 <adenaikeeunice@gmail.com>
2023-01-02 10:43:48 -0800

    more pointers and arrays

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,12 @@
  */
 char*_memset(cha*s, char b, unsigned int n)
 {
-	int i = 0
+	unsigned int i = 0
 
-	for(;n >0; i++)
+	while (1 < n)
 	{
-		s[i] = b
-			n-;
+		s[i] = b;
+		i++;
 	}
 	return (s);
 }
